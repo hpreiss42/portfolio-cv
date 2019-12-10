f497f4fea3d50543515190f4c94f23b6def28d04
Heather <heather.l.preiss@gmail.com>
2019-12-10 12:49:32 -0600

    updates to styles, components, info

---
+++ data/siteConfig.js
@@ -0,0 +1,135 @@
+module.exports = {
+  siteTitle: 'Hello! I\'m Heather!',
+  siteDescription: `Web Developer CV`,
+  keyWords: ['gatsbyjs', 'react', 'curriculum'],
+  authorName: 'Heather Preiss',
+  twitterUsername: '',
+  githubUsername: 'hpreiss42',
+  authorAvatar: '/images/avatar.jpg',
+  authorDescription: `Developer, passionate about what I do. Always interested in how the sites were made, I started to study HTML by hobby. <br />
+    In 2012 I started working as a support technician and I approached the developers.
+    In 2015, I started to study C # and started to contribute with the team giving maintenance in an application in C # and .NET. <br />
+    I currently work as a frontend developer and mainly work with <strong>Javascript, NodeJS e React.</strong>`,
+  skills: [
+    {
+      name: 'HTML',
+      level: 70
+    },
+    {
+      name: 'CSS',
+      level: 70
+    },
+    {
+      name: 'Javascript',
+      level: 40
+    },
+    {
+      name: 'React',
+      level: 35
+    },
+    {
+      name: 'Photoshop',
+      level: 70
+    },
+    {
+      name: 'Adobe XD',
+      level: 40
+    }
+  ],
+  jobs: [
+    {
+      company: "Noisy Trumpet",
+      begin: {
+        month: 'May',
+        year: '2019'
+      },
+      duration: null,
+      occupation: "Web Developer Intern",
+      description: "..."
+    },
+    {
+      company: "Corona Visions",
+      begin: {
+        month: 'Oct',
+        year: '2017'
+      },
+      duration: 'August 2018',
+      occupation: "Electronic & Equipment Recycling Technician",
+      description: "..."
+    },
+    {
+      company: "IBEX Global",
+      begin: {
+        month: 'Mar',
+        year: '2017'
+      },
+      duration: 'May 2017',
+      occupation: "Customer Support - Tier 1 Technical Support Agent",
+      description: "..."
+    },
+    /* ... */
+  ],
+  schools: [
+    {
+      name: "Northwest Vista College",
+      begin: {
+        month: 'Jun',
+        year: '2018'
+      },
+      duration: null,
+      major: "A.A.S. Digital Media",
+      description: "..."
+    },
+    {
+      name: "Alamo Community Colleges",
+      begin: {
+        month: 'Sep',
+        year: '2017'
+      },
+      duration: 'to November 2017',
+      major: "CompTIA A+ and Network+",
+      description: "..."
+    },
+    {
+      name: "Rackspace Open Cloud Academy",
+      begin: {
+        month: 'Jun',
+        year: '2017'
+      },
+      duration: '8.5 weeks',
+      major: "Linux System Administration",
+      description: "..."
+    },
+    {
+      name: "Northwest Vista College",
+      begin: {
+        month: 'Aug',
+        year: '2012'
+      },
+      duration: 'May 2015',
+      major: "A.A.S. 3D Animation",
+      description: "..."
+    },
+    /* ... */
+  ],
+  social: {
+    twitter: "",
+    linkedin: "",
+    github: "https://github.com/hpreiss42",
+    email: ""
+  },
+  siteUrl: 'https://hpreiss42.github.io/portfolio-cv/',
+  pathPrefix: '/portfolio-cv', // Note: it must *not* have a trailing slash.
+  siteCover: '/images/cover.jpg',
+  googleAnalyticsId: '',
+  background_color: '#ffffff',
+  theme_color: '#2f253b',
+  display: 'minimal-ui',
+  icon: 'src/assets/gatsby-icon.png',
+  headerLinks: [
+    {
+      label: 'Heather Preiss',
+      url: '/',
+    }
+  ]
+}
---
+++ src/components/styles/global-style.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  ${'' /* put your global css here */}
+
+  #pageBG {
+  	background-color: #e1e1e1;
+  	margin-bottom: -2%;
+  }
+
+`
